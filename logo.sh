a6db9d78931b7f00c3b0786b056cb597219202db
Benny13017 <127016420+Benny13017@users.noreply.github.com>
2024-02-18 17:56:27 +0800

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -7,3 +7,12 @@ echo "| |_) |  __/ | | | | | | |_| |"
 echo "|____/ \___|_| |_|_| |_|\__, |"
 echo "                        |___/" 
 echo "Benny一键脚本工具 v1.0 （支持Ubuntu，Debian，Centos系统）"
+
+
+
+
+echo "BBBBB    EEEEE  N   N  N   N  Y   Y"
+echo "B    B   E      NN  N  NN  N   Y Y"
+echo "BBBBB    EEEE   N N N  N N N    Y"
+echo "B    B   E      N  NN  N  NN    Y"
+echo "BBBBB    EEEEE  N   N  N   N    Y"
